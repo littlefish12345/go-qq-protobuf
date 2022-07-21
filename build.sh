70b8cc6cd715cb2520b825600076e3a997e68cc0
littlefish12345 <18819303050@163.com>
2022-07-21 11:20:04 +0800

    修改protobuf生成方式以及结构

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-protoc ./protobufStruct.proto --gogofaster_out ./
+protoc ./protobufStruct.proto --go_out ./
 cp ./github.com/littlefish12345/go-qq-protobuf/protobufStruct.pb.go .
 rm -r github.com
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/littlefish12345/go-qq-protobuf
 
 go 1.18
 
-require github.com/gogo/protobuf v1.3.2 // indirect
+require google.golang.org/protobuf v1.28.0
